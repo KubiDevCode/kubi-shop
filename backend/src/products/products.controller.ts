c7b1d7010588fe4ba7dd954413a06ddf4dfcc9c6
Kubi <stepancrazy89@gmail.com>
2026-05-05 23:36:17 +0300

    добавил в контроллер

--- backend/src/products/products.controller.ts
+++ backend/src/products/products.controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
-  constructor(private readonly productsService: ProductsService) {}
+  constructor(private readonly productsService: ProductsService) { }
 
   @Get()
   findAll() {
@@ -13,5 +13,13 @@ export class ProductsController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.productsService.findOne(id);
+  }
+
+  @Get()
+  findPage(
+    @Query('page') page: number,
+    @Query('limit') limit: number
+  ) {
+    return this.productsService.findPage(page, limit)
   }
 }
