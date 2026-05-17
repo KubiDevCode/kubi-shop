00c531144cd4540d8b4ca3563ed689cf17efc03b
Kubi <stepancrazy89@gmail.com>
2026-05-17 23:21:30 +0300

    сделал фильтрацию по цене, добавил onInputChange в компонент фильтра, удалил неиспользуемый код

--- frontend/kubi-shop/src/features/FilterProducts/types/filterProductsTypes.ts
+++ frontend/kubi-shop/src/features/FilterProducts/types/filterProductsTypes.ts
@@ -6,28 +6,28 @@ export type FilterType = 'category' | 'brand' | 'tag'
 export type PriceFilterAction = 'minPrice' | 'maxPrice'
 
 export interface FilterItem {
-    id: number
-    title: string
-    data: CategoryNameType | BrandNameType | TagNameType
+  id: number
+  title: string
+  data: CategoryNameType | BrandNameType | TagNameType
 }
 
 export interface ShopFilter {
-    id: number
-    title: string
-    type: FilterType
-    filters: FilterItem[]
+  id: number
+  title: string
+  type: FilterType
+  filters: FilterItem[]
 }
 
 export interface InputFilterItem {
-    id: number
-    placeholder: string
-    title: string
-    defValue?: string
-    action: PriceFilterAction
+  id: number
+  placeholder: string
+  title: string
+  defValue?: string
+  action: PriceFilterAction
 }
 
 export interface InputShopFilter {
-    id: number
-    title: string
-    filters: InputFilterItem[]
+  id: number
+  title: string
+  filters: InputFilterItem[]
 }
